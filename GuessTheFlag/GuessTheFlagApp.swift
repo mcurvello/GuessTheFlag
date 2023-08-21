66c57ce6fb5aa9a1e8543fef5c0860d550d19c17
Marcio Curvello <marciocurvello@take.net>
2023-08-20 21:15:43 -0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Marcio Curvello on 20/08/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
